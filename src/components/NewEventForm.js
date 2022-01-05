d5c889d52f4ed9606b5161dff4b8080297f1fed7
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-05 14:39:07 +0200

    The onChange Event

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import './NewEventForm.css';
 
 export default function NewEventForm() {
+	const [formData, setFormData] = useState({
+		title: '',
+		date: ''
+	});
+
+	const handleChange = (e) => {
+		setFormData((prevFormData) => {
+			return { ...prevFormData, [e.target.name]: e.target.value };
+		});
+	};
+
 	return (
 		<form className="new-event-form">
 			{/* <label htmlFor="title">Event Title:</label>
       <input type="text" id="title" /> */}
 			<label>
 				<span>Event Title:</span>
-				<input type="text" />
+				<input type="text" onChange={handleChange} name="title" />
 			</label>
 			<label>
 				<span>Event Date:</span>
-				<input type="date" />
+				<input type="date" onChange={handleChange} name="date" />
 			</label>
+			<p>
+				{formData.title} {formData.date}
+			</p>
 			<button>Submit</button>
 		</form>
 	);
